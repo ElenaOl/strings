d924639663f63dc1e64b5f8b91464512133e6fb8
ElenaOl <pravutiner@gmail.com>
2018-02-16 13:24:02 -0800

    question 2 better solution

--- replace-all-spaces.js
+++ replace-all-spaces.js
@@ -1,4 +1,5 @@
 // 3. Write a method to replace all spaces in a string with ‘%20’.
+// O(n)
 function replace(str){
     str = str.split('');
     for(var i=0; i<str.length; i++){
--- string-compression.js
+++ string-compression.js
@@ -1,26 +1,27 @@
 // 4. Implement a method to perform a basic string compression using the counts of 
 // repeated characters. For example, the string aabccccaaa would become a2b1c4a3. 
-// If the compressed string would not become smaller than the original string, your method 
-// should return the original string.
+// If the compressed string would not become smaller than the original string, your method should return the original string.
 
 function compression(str){
-    
-    var resultStr = "";
-    var count = 1;
-    var current = str[0];
-    for (var i=1; i<=str.length; i++){
-      if(current === str[i]){
-        count += 1;
-      }else{
-        resultStr += (current + count);
-        current = str[i];
-        count = 1;
-      }
+  
+  var resultStr = "";
+  var count = 1;
+  var current = str[0];
+  for (var i=1; i<=str.length; i++){
+    if(current === str[i]){
+      count += 1;
+    }else{
+      resultStr += (current + count);
+      current = str[i];
+      count = 1;
     }
-    
-    return resultStr;
-    
+  }
+  if(str.length <= resultStr.length){
+    return str;
   }
   
-  compression('aabccccaaa'); //a2b1c4a3
-  
+  return resultStr;
+ 
+}
+
+compression('abcdef'); //a2b1c4a3
