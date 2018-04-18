eb1c4a90db03bce16fb685ed808e56f8025ef805
ElenaOl <pravutiner@gmail.com>
2018-04-18 11:00:26 -0700

    improvements to 6

--- longest-common-subsequence.js
+++ longest-common-subsequence.js
@@ -4,28 +4,27 @@
 
 // O(2n)
 
+
 function common(str1, str2){
   var arrOfsubsequence = [];
   var h1 = {};
   
    for(let i=0; i<str1.length; i++){
       if(h1[str1[i]] !== undefined ){
-        h1[str1[i]] = 1;
+        h1[str1[i]] += 1;
       }else{
         h1[str1[i]] = 1;  
       }
   }
   for(let j=0; j<str2.length; j++){
-      if(h1[str2[j]] !== undefined){
+      if(h1[str2[j]] !== undefined && h1[str2[j]] > 0){
         arrOfsubsequence.push(str2[j]);
-        h1[str2[j]] === undefined;
+        h1[str2[j]] -=1;
       } 
   }
   // console.log(arrOfsubsequence)
- 
-  return arrOfsubsequence.length;
+  var subsequence = arrOfsubsequence.join('');
+  return subsequence;
 }
-common('ABCDGH', 'AEDFHR');
-
-
+common('AGGTAB', 'GGGXTXAYB');
 
