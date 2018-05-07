aa07d6dffdb4ba3f8ad434a007b9c04199c1b637
ElenaOl <pravutiner@gmail.com>
2018-05-07 09:31:14 -0700

    additional solution q 7

--- shortest-str-includes-overlap.js
+++ shortest-str-includes-overlap.js
@@ -18,3 +18,48 @@ function subsequences(str1, str2){
   return undefined;
 }
 subsequences('abc', 'eke');
+
+function isSubsequence (str1, str2, count){
+  if(count > str1.length || count > str2.length){
+    return '';
+  }
+  var res = isSubsequence(str1, str2, count+1);
+  if(res !== ''){
+    return res;
+  }
+
+  for(var i= 0; i<count; i++){
+    if(str1[str1.length-count+i] !== str2[i]){
+      return '';
+    }
+  }
+  res = str1.slice(0, -count) + str2;
+  return res;
+   
+}
+var str1 = 'geek';
+var str2 = 'eke';
+isSubsequence (str1, str2, 1);
+
+function isPrefix (str1, str2, count){
+  if(count >= str1.length){
+    return '';
+  }
+
+  if (str1.length - count > str2.length){
+    return isPrefix(str1, str2, count+1);
+  }
+
+  for(var i=count; i<str1.length; i++){
+    if(str1[i] !== str2[i - count]){
+      return isPrefix(str1, str2, count+1);
+    }
+  }
+
+  console.log(count);
+  res = str1.slice(0 , count - str1.length) + str2;
+  return res;
+}
+var str1 = 'dabab';
+var str2 = 'ababv';
+isPrefix (str1, str2, 0);
